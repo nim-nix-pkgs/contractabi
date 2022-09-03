6de5ba0e8bdf7f8995245c8dafcd38f7f063ce52
hugosenari <hugosenari@gmail.com>
2022-09-03 19:56:13 -0300

    feat(nimpkgs): update lock

--- source/contractabi.nim
+++
@@ -1,7 +0,0 @@
-import contractabi/encoding
-import contractabi/decoding
-import contractabi/selector
-
-export encoding
-export decoding
-export selector
--- source/tests/testAll.nim
+++
@@ -1,7 +0,0 @@
-import ./contractabi/testAddress
-import ./contractabi/testEncoding
-import ./contractabi/testDecoding
-import ./contractabi/testCustomTypes
-import ./contractabi/testSelector
-
-{.warning[UnusedImport]:off.}
